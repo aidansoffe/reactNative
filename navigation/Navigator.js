bb4fea58254b861fb5da2f2e5bf65544ed1f6d74
Moon Munar <moonmunar@Moons-MBP.attlocal.net>
2019-06-02 20:28:25 -0500

    images render

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -2,11 +2,13 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Main from '../src/Main';
 import Home from '../src/Home';
 import Profile from '../src/Profile';
+import Resource from '../src/Resource';
 
 const RootStack = createStackNavigator({
   Home: { screen: Home },
   Main: { screen: Main },
-  Profile: { screen: Profile}
+  Profile: { screen: Profile},
+  Resource: { screen: Resource},
 });
 
 const AppNavigator = createAppContainer(RootStack);
--- src/Profile.js
+++ src/Profile.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 
 export default class Profile extends Component {
     render() {
         return (
-          <View>
-              <Text> Moya Koyoyeva!!  </Text>
-              
-          </View>  
-        )
+            <View style={Look.container}>
+                <Text style={Look.Text}> Moya Koyoyeva!!  </Text>
+                <View style={Look.goBackButton}>
+                <TouchableOpacity
+                style={Look.button}
+                        onPress={() => this.props.navigation.navigate('Resource')}
+                    >
+                        <Text>Go Back Home</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        );
     }
 }
+const Look = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+        // marginBottom: 0,
+        justifyContent: 'center',
+        backgroundColor: '#ff69b4',
+        alignItems: 'center',
+        width: '100%'
+    },
+    Text: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        fontFamily: 'Cochin'
+    },
+    goBackButton: {
+        marginTop: 90,
+        // backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%'
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+        padding: 10
+    }
 
-
+});
